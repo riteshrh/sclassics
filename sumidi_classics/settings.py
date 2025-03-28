009c4f12502f4f1e808476f599c6a5e70705ff04
Ritesh <honnalliritesh43@gmail.com>
2025-03-28 03:19:14 -0400

    code fix

--- sumidi_classics/settings.py
+++ sumidi_classics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fcoo%da42y-r))adpo$=9mi)7fe&_%ptt^155!i=@olbxus)gm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sclassics.onrender.com','sumidiclassics.in']
+ALLOWED_HOSTS = ['sclassics.onrender.com','sumidiclassics.in', 'localhost']
 
 
 # Application definition
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
-    'tailwind'
+    'tailwind',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
