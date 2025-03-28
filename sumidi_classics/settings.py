56e409968bae3b9df2105e3e5ab1b754abde8862
Ritesh <honnalliritesh43@gmail.com>
2025-03-28 04:16:08 -0400

    last_try_css

--- sumidi_classics/settings.py
+++ sumidi_classics/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
