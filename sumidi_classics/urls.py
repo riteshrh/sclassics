c433d1e893ad8ff0df1eb90442e71aae69ee0716
Ritesh <honnalliritesh43@gmail.com>
2025-03-28 14:21:49 -0400

    image_field_update

--- sumidi_classics/urls.py
+++ sumidi_classics/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from shop import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('shop/<str:category>/', views.category_products, name='category_products'),
     path('faq/', views.faq, name='faq'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
